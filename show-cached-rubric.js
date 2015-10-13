48a5b135130ae4cb7b4b8634e5da015a2314df57
Seth Battis <sethbattis@stmarksschool.org>
2015-10-13 12:40:21 -0400

    Asynchronous XMLHttpRequest
    It seems that, when provided with a dynamically-defined function, XMLHttpRequests behave synchronously, but when provided with a reference to a statically-defined function, XMLHttpRequests are, as intended, fully asynchronous. Who knew? (Or, more importantly, bothered to write in the bleeping documentation?)

--- show-cached-rubric.js
+++ show-cached-rubric.js
@@ -6,27 +6,23 @@ function stmarks_showCachedRubric() {
 		return;
 	}
 	
+	function displayCache() {
+		if (http.readyState === 4 && http.status === 200) {
+			var rubrics = document.createElement('div');
+			var tool = $('.tool_content_wrapper')[0];
+			tool.parentNode.insertBefore(rubrics, tool);
+			rubrics.outerHTML = http.responseText;
+			rubrics.style.display = 'block';
+		}
+	}
+	
 	var url = 'https://skunkworks.stmarksschool.org/canvas/lti/canvashack/hacks/cache-and-show-rubrics/show-rubrics.php';
 	var http = new XMLHttpRequest();
 	var params = 'assignment=' + encodeURIComponent(assignment);
-	http.open('POST', url, true);
+	http.open('POST', url);
 	http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	http.setRequestHeader("Content-length", params.length);
-	http.setRequestHeader("Connection", "close");
-	http.onreadystatechange = function() {
-		if (http.readyState === 4 && http.status === 200) {
-			var rubrics = document.createElement('div');
-			var footer = document.getElementById('module_sequence_footer');
-			var tool = document.getElementById('tool_content');
-			footer.parentNode.insertBefore(rubrics, footer);
-			//document.getElementById('content').removeClass('padless');
-			rubrics.outerHTML = http.responseText;
-			rubrics.style.display = 'block';
-			if (tool.src == 'about:blank') {
-				tool.style.height = '0px';
-				tool.parentNode.style.height = 'auto';
-			}
-		}
-	};
+	//http.setRequestHeader("Content-length", params.length);
+	//http.setRequestHeader("Connection", "close");
+	http.addEventListener('load', displayCache);
 	http.send(params);
 }
